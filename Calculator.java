b8d9bcbea9995bb281bd5b980153fc6d5dae77bb
amansingh977 <amansinghrajawat09@gmail.com>
2025-01-07 12:32:19 +0530

    add

--- Calculator.java
+++ Calculator.java
@@ -3,5 +3,9 @@ class Calculator
 public static void main(String[] args)
 {
      System.out.println("This is calculator");
+     add(123, 45);
+}
+public static add(int a, int b) {
+     System.out.println(a+b);
 }
 }

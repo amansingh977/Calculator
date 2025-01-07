ecd39a7c3a0a5e812f9cda10647c05e23fa90357
amansingh977 <amansinghrajawat09@gmail.com>
2025-01-07 13:00:43 +0530

    multiply

--- Calculator.java
+++ Calculator.java
@@ -5,11 +5,15 @@ public static void main(String[] args)
      System.out.println("This is calculator");
      add(123, 45);
      subtract(15, 5);
+     multiply(11, 33);
 }
-public static add(int a, int b) {
+public static void add(int a, int b) {
      System.out.println(a+b);
 }
-public static subtract(int a, int b) {
+public static void subtract(int a, int b) {
           System.out.println(a-b);
-     }
+}
+public static void multiply(int a, int b) {
+          System.out.println(a*b);
+}
 }

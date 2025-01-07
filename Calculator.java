f104b0df5390498d235a4b4bb365fd8334793611
amansingh977 <amansinghrajawat09@gmail.com>
2025-01-07 12:40:04 +0530

    sub

--- Calculator.java
+++ Calculator.java
@@ -4,8 +4,12 @@ public static void main(String[] args)
 {
      System.out.println("This is calculator");
      add(123, 45);
+     subtract(15, 5);
 }
 public static add(int a, int b) {
      System.out.println(a+b);
 }
+public static subtract(int a, int b) {
+          System.out.println(a-b);
+     }
 }
